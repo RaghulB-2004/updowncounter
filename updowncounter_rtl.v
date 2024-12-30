885fd1f6cea8c5b0c68d59d26b50693f9cd86109
RaghulB-2004 <raghulraghunathb@gmail.com>
2024-12-30 19:56:18 +0530

    Add files via upload

---
+++ updowncounter_rtl.v
@@ -0,0 +1,20 @@
+module updowncounter_rtl(rst,clk,updown,count);
+input rst;
+input clk;
+input updown;
+output reg [3:0] count;
+always@(posedge clk)
+begin
+if(rst!=0) begin
+count<=4'b0000;
+end
+else begin
+if(updown==1)begin
+count<=count+1'b1;
+end
+else begin
+count<=count-1'b1;
+end
+end
+end
+endmodule
---
+++ updowncounter_tb.v
@@ -0,0 +1,27 @@
+module updowncounter_tb;
+reg clk;
+reg rst;
+reg updown;
+wire [3:0]count;
+updowncounter_rtl uut (
+.clk(clk),
+.rst(rst),
+.updown(updown),
+.count(count)
+);
+always #5 clk = ~clk;
+initial begin
+clk = 0;
+rst= 1;
+updown= 0;
+#10 rst = 0;
+#10 updown = 1;
+repeat (15) begin
+#10 updown = 1;
+end
+repeat (15) begin
+#10 updown = 0;
+end
+#20 $finish;
+end
+endmodule
